c463011cdf280bb1076545c5f4c3476f909613a6
monirul360 <monirulislam65361@gmail.com>
2022-05-01 20:40:34 +0600

    error404 page setup

--- src/Page/Error/Error.js
+++ src/Page/Error/Error.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import './Error.css';
 const Error = () => {
     return (
         <div>
-            <h2>Error 404</h2>
-        </div>
+            <div className="error-container">
+                <div className="error">
+                    <h1 id='oops'>Oops !</h1>
+                    <h1>404</h1>
+                    <h2>Page not found</h2>
+                </div>
+            </div>
+        </div >
     );
 };
 
