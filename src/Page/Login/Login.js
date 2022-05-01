248cb01e11b8d3b0a55d0a5d242e8e9987755a42
monirul360 <monirulislam65361@gmail.com>
2022-05-02 01:09:20 +0600

    login page design

--- src/Page/Login/Login.js
+++ src/Page/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Login.css';
 const Login = () => {
     return (
         <div>
-            <h2>This is login page</h2>
+            <section>
+                <div className="from-container">
+                    <div className="from">
+                        <h2 id='from-title'>Please Login</h2>
+                        <form >
+                            <input type="text" name="text" placeholder='Enter your email' id="" />
+                            <input type="password" name="password" placeholder='Enter your password' id="" />
+                            <input type="submit" value="Login" />
+                        </form>
+                        <p>Booking Travel guide? <Link id='Register' to='/signup'>Please Register</Link></p>
+                        <p>Forget Password? <span id='reset-pass'>Reset Password</span></p>
+                        <div className="or-flex">
+                            <div></div>
+                            <h4>OR</h4>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
