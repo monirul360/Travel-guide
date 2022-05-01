6d3a060e09af99263a54bf7712cc57e1875cb59d
monirul360 <monirulislam65361@gmail.com>
2022-05-02 00:32:03 +0600

    footer page complate

--- src/Page/Share/Footer/footer.js
+++ src/Page/Share/Footer/footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
+import fb from '../../../Image/footer/facebook.png';
+import tw from '../../../Image/footer/Twitter.png';
+import li from '../../../Image/footer/linkedin.png';
+import go from '../../../Image/footer/instagram.png';
 const Footer = () => {
     return (
         <div>
             <footer>
                 <section>
                     <div className="footer-container">
+                        <div className="footer-social">
+                            <ul>
+                                <li><Link to='/'><img src={fb} alt="" /></Link></li>
+                                <li><Link to='/'><img src={tw} alt="" /></Link></li>
+                                <li><Link to='/'><img src={li} alt="" /></Link></li>
+                                <li><Link to='/'><img src={go} alt="" /></Link></li>
+                            </ul>
+                        </div>
                         <div className="footer-flex">
                             <div className='footer-contact'>
                                 <h2>CONTACT US </h2>
@@ -31,8 +43,14 @@ const Footer = () => {
                                     <input type="text" name="name" placeholder='Name' id="" />
                                     <input type="email" placeholder='Email' name="email" id="" />
                                     <textarea name="" id="" placeholder='Comment' ></textarea>
+                                    <br />
+                                    <input type="submit" value="Submit" />
                                 </div>
                             </div>
+                        </div>
+                        <div className="copy-right">
+                            <hr />
+                            <p>Copyright &copy; 2022 Travel guide All Right Reserved</p>
                         </div>
                     </div>
                 </section>
